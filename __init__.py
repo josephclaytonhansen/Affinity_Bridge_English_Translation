a9324f9ad131e3d7b053ecdba7c8cf4fab16cdf5
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-01-20 07:55:25 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,23 +5,23 @@
 #
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 bl_info = {
-    "name" : "Affinity_Bridge",
-    "author" : "Hattori_Kaoru( t0rry_ )",
-    "description" : "BlenderからAffinityPhoto2を起動します",
-    "blender" : (4, 0, 0),
-    "version" : (0, 6, 0),
-    "location" : "Image Editor >> Property Panel",
-    "warning" : "This vertions experiment",
-    "support" : "COMMUNITY",
-    "category" : "User",
-    "doc_url" : "https://github.com/t0rry/Affinity_Bridge"
+    "name": "Affinity_Bridge",
+    "author": "Hattori_Kaoru( t0rry_ )",
+    "description": "Launches AffinityPhoto2 from Blender",
+    "blender": (4, 0, 0),
+    "version": (0, 6, 0),
+    "location": "Image Editor >> Property Panel",
+    "warning": "This version is experimental",
+    "support": "COMMUNITY",
+    "category": "User",
+    "doc_url": "https://github.com/t0rry/Affinity_Bridge"
 }
 
 if "bpy" in locals():
@@ -43,99 +43,100 @@ from bpy.props import (
 )
 
 class AffinityBridgeProp(bpy.types.PropertyGroup):
-    file_format:EnumProperty(
-        name = 'file_fromat',
-        description = 'file_format',
-        items = [('PNG','PNG','select PNG(file format)'),
-                ('JPEG','JPEG','select JPEG(file format)'),
-                ('OPEN_EXR','Open_EXR','select OpenEXR(file format)'),
-                ('OPEN_EXR_MULTILAYER','Open EXR Multilayer','select OpenEXR_Multilayer(file format)')]
+    file_format: EnumProperty(
+        name='File Format',
+        description='Choose the file format',
+        items=[('PNG', 'PNG', 'PNG file format'),
+               ('JPEG', 'JPEG', 'JPEG file format'),
+               ('OPEN_EXR', 'Open_EXR', 'OpenEXR file format'),
+               ('OPEN_EXR_MULTILAYER', 'Open EXR Multilayer', 'OpenEXR Multilayer file format')]
     )
     
-    color_mode:EnumProperty(
-        name = 'color_mode',
-        description ='color mode',
-        items = [('BW', "BW",'select BW(color mode)'),
-                ('RGB','RGB','select RGB(color mode)'),
-                ('RGBA','RGBA','select RGBA(color mode)')]
+    color_mode: EnumProperty(
+        name='Color Mode',
+        description='Choose the color mode',
+        items=[('BW', "Black & White", 'Black & White color mode'),
+               ('RGB', 'RGB', 'RGB color mode'),
+               ('RGBA', 'RGBA', 'RGBA color mode')]
     )
     
-    file_name:StringProperty(
-        name = 'file_name',
-        description = 'use to saved image',
-        default = 'AffinityBridge'
+    file_name: StringProperty(
+        name='File Name',
+        description='Name to use for saved image',
+        default='AffinityBridge'
     )
     
-    path_str:StringProperty(
-        name = 'path_string',
-        description = 'display of saved image path',
-        default = ''
+    path_str: StringProperty(
+        name='Path String',
+        description='Display of saved image path',
+        default=''
     )    
     
-    is_change_name:BoolProperty(
-        name = 'change_name',
-        description = 'use to input orignal name',
-        default = False
+    is_change_name: BoolProperty(
+        name='Change Name',
+        description='Use to input original name',
+        default=False
     )
 
-    is_used_affinityphoto:BoolProperty(
-        name = 'AffinityPhotoを利用する',
-        description = 'AffinityBridgeするときにAffinityPhotoV2が起動されます',
-        default = True
+    is_used_affinityphoto: BoolProperty(
+        name='Use AffinityPhoto',
+        description='Launch AffinityPhotoV2 when using AffinityBridge',
+        default=True
     )
-    #maybe dont use old_ff,old_cm
+    # maybe don't use old_ff, old_cm
     
-    old_ff:StringProperty(
-        name = 'old file_format',
-        description = 'to changing parameter when saved image'
+    old_ff: StringProperty(
+        name='Old File Format',
+        description='To change parameter when saving image'
     )
     
-    old_cm:StringProperty(
-        name = 'old color_mode',
-        description = 'to changing parameter when saved image'        
+    old_cm: StringProperty(
+        name='Old Color Mode',
+        description='To change parameter when saving image'        
     )
     
 class AFFINITYBRIDGE_MT_CompositPanel(bpy.types.Menu):
     
     bl_idname = "AFFINITYBRIDGE_MT_SetOpenExrSelected"
-    bl_label = "AffnityBridge"
-    bl_description = "コンポジット上で機能するパネル"
+    bl_label = "AffinityBridge"
+    bl_description = "Panel that works on the Compositor"
     
-    def draw(self,context):
+    def draw(self, context):
         layout = self.layout
         ui_type = context.area.ui_type
         if ui_type == "CompositorNodeTree":
-            layout.operator(ot.AFFINTYBRIDGE_OT_SetOpenEXR_Selected.bl_idname,icon='NODETREE')  
-            layout.operator(ot.AFFINITYBRIDGE_OT_SetOpenEXR_RenderLayer.bl_idname,icon='NODETREE')               
+            layout.operator(ot.AFFINTYBRIDGE_OT_SetOpenEXR_Selected.bl_idname, icon='NODETREE')  
+            layout.operator(ot.AFFINITYBRIDGE_OT_SetOpenEXR_RenderLayer.bl_idname, icon='NODETREE')               
 
 class AFFINITYBRIDGE_Preferences(bpy.types.AddonPreferences):
     bl_idname = __name__
 
         
-    option_image_editing_exe:StringProperty(
-        name = "画像編集ソフトを指定",
-        subtype = "FILE_PATH",
-        default = ""
+    option_image_editing_exe: StringProperty(
+        name="Specify Image Editing Software",
+        subtype="FILE_PATH",
+        default=""
     )
     
-    is_display_filepath:BoolProperty(
-        name = "UI上にexeファイルのファイルパス指定を表示する",
-        default = False
+    is_display_filepath: BoolProperty(
+        name="Display file path of exe file on UI",
+        default=False
     )
     
-    def draw(self,context):
+    def draw(self, context):
         layout = self.layout
         
         box = layout.box()
         box.prop(self, 'option_image_editing_exe')
-        box.prop(self,'is_display_filepath',text = "画像エディタ上でexeファイルを編集できるようにする")
-        box.label(icon = 'ERROR',text = 'イメージエディタ上にファイルパスが表示されるため無効にすることを推奨します。')
+        box.prop(self, 'is_display_filepath', text="Enable editing exe file on the image editor")
+        box.label(icon='ERROR', text='It is recommended to disable this, as the file path will be displayed on the image editor.')
+        
 def menu_register_func(cls, context):
     
     ui_type = context.area.ui_type
     if ui_type == "CompositorNodeTree":
         cls.layout.separator()
-        cls.layout.menu(AFFINITYBRIDGE_MT_CompositPanel.bl_idname, icon = 'NODETREE')
+        cls.layout.menu(AFFINITYBRIDGE_MT_CompositPanel.bl_idname, icon='NODETREE')
         
 classes = [
     AffinityBridgeProp,
@@ -153,7 +154,7 @@ classes = [
 def register():
     for c in classes:
         bpy.utils.register_class(c)
-    bpy.types.Scene.affinitybridge = bpy.props.PointerProperty(type = AffinityBridgeProp)
+    bpy.types.Scene.affinitybridge = bpy.props.PointerProperty(type=AffinityBridgeProp)
     bpy.types.NODE_MT_context_menu.append(menu_register_func)
     
 def unregister():
@@ -161,4 +162,3 @@ def unregister():
     for c in classes:
         bpy.utils.unregister_class(c)
     del bpy.types.Scene.affinitybridge
-
